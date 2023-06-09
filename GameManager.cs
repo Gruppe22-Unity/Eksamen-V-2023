72870ccf70a79000fd46a46912533f5e407e996a
Gruppe22-Unity <246735@usn.no>
2023-06-09 11:30:27 +0200

    Deloppg 4: "Game Health" implementeres, og blir satt til samme tall som credit. La også til level nummer og score.

---
+++ GameManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public UIScripts uiScripts;
+
+    private int gameHealth;
+    private int levelNumber;
+    private int score;
+
+    private void Start()
+    {
+        InitializeGame();
+    }
+
+    private void InitializeGame()
+    {
+        // Sett opp startverdier for game health, levelnummer og score
+        gameHealth = 3;
+        levelNumber = 1;
+        score = 0;
+
+        // Oppdater UI
+        uiScripts.UpdateGameHealthUI(gameHealth);
+        uiScripts.UpdateLevelNumberUI(levelNumber);
+        uiScripts.UpdateScoreUI(score);
+    }
+    public void IncreaseLevelNumber()
+    {
+        levelNumber++;
+
+        // Oppdater UI
+        uiScripts.UpdateLevelNumberUI(levelNumber);
+    }
+
+    public void IncreaseScore(int amount)
+    {
+        score += amount;
+
+        // Oppdater UI
+        uiScripts.UpdateScoreUI(score);
+    }
+    public void DecreaseGameHealth(int amount)
+    {
+        gameHealth -= amount;
+
+        // Oppdater UI
+        uiScripts.UpdateGameHealthUI(gameHealth);
+
+        if (gameHealth <= 0)
+        {
+            // Spilleren har mistet all helse, håndter spillerens død her
+            GameOver();
+        }
+    }
+
+    private void GameOver()
+    {
+        Debug.Log(" Game Over 🙁 ");
+    }
+}
---
+++ UIScripts.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScripts : MonoBehaviour
+{
+    public Text gameHealthText;
+    public Text levelNumberText;
+    public Text scoreText;
+
+    public void UpdateGameHealthUI(int health)
+    {
+        gameHealthText.text = "Game Health: " + health.ToString();
+    }
+
+    public void UpdateLevelNumberUI(int level)
+    {
+        levelNumberText.text = "Level: " + level.ToString();
+    }
+
+    public void UpdateScoreUI(int score)
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+}
